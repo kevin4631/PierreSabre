a774dbd3d4722961145d51e0731b52bb664911b7
kevin <kevinrochetaing46@gmail.com>
2022-11-07 22:51:37 +0100

    création class Humain

---
+++ src/personnages/Humain.java
@@ -0,0 +1,57 @@
+package personnages;
+
+public class Humain {
+
+	private String nom;
+	private String boisson;
+	private int argent;
+
+	public Humain(String nom, String boisson, int argent) {
+		super();
+		this.nom = nom;
+		this.boisson = boisson;
+		this.argent = argent;
+	}
+
+	private void parler(String text) {
+		System.out.println("(" + this.getNom() + ") - " + text);
+	}
+
+	public void direBonjour() {
+		this.parler("Bonjour ! Je m'appelle " + this.getNom() + " et j'aime boire du " + this.boisson);
+	}
+
+	public void boire() {
+		this.parler("Mmmm, un bon verre de " + this.boisson + " ! GLOUPS !");
+	}
+
+	public void acheter(String bien, int prix) {
+		if (this.argent >= prix) {
+			this.parler("j'ai " + this.getArgent() + " sous en poche. Je vais pouvoir m'offrir une " + bien + " à "
+					+ prix + " sous");
+			this.argent -= prix;
+		} else {
+			this.parler(" Je n'ai plus que " + this.argent + " sous en poche. Je ne peux même pas m'offrir un kimono à "
+					+ prix + " sous.");
+		}
+	}
+
+	public int gagnerArgent(int somme) {
+		this.argent += somme;
+		return this.getArgent();
+	}
+
+	public int perdreArgent(int somme) {
+		this.argent -= somme;
+		return this.getArgent();
+	}
+
+	public String getNom() {
+		return this.nom;
+	}
+
+	public int getArgent() {
+		return this.argent;
+	}
+
+}
